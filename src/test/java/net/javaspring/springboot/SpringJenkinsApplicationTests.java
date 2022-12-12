ae277783af9fae40d63a5b3e6641c175d79df25b
PUNOGOTI VIDYA SAGARR <36266513+PunugotiVidya@users.noreply.github.com>
2022-12-12 10:58:49 +0530

    Update SpringJenkinsApplicationTests.java

--- src/test/java/net/javaspring/springboot/SpringJenkinsApplicationTests.java
+++ src/test/java/net/javaspring/springboot/SpringJenkinsApplicationTests.java
@@ -1,6 +1,7 @@
 package net.javaspring.springboot;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
